0f947885817fdbe0cd6152dba44c4b841c080b35
mochamadgalih <mochamadglh@gmail.com>
2024-05-27 17:26:08 +0700

    fix bug

--- models/campground.js
+++ models/campground.js
@@ -40,10 +40,10 @@ const CampgroundSchema = new Schema({
   ],
 });
 
-CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
-  return `<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong> 
-  <p>${this.description.substring(0, 20)}...</p>`;
-});
+// CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+//   return `<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+//   <p>${this.description.substring(0, 20)}...</p>`;
+// });
 
 //! ini middlewarenya.
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
